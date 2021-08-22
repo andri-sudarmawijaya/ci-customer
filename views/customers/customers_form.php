aa2d730d921db44b0b48f146b8c8f83236d51640
Andri Sudarmawijaya <andri.sudarmawijaya@gmail.com>
2021-08-22 11:48:13 +0700

    remove readonly

--- views/customers/customers_form.php
+++ views/customers/customers_form.php
@@ -51,73 +51,73 @@ defined('BASEPATH') or exit('No direct script access allowed');
 								<div role="group" class="form-group form-row">
 									<label for="CustomerName" class="col-form-label col-sm-3">CustomerName</label>
 									<div class="col-sm-9">
-										<input id="CustomerName" type="text" readonly="readonly" class="form-control-plaintext" value="<?php echo $customerName; ?>"">
+										<input id="CustomerName" type="text"  class="form-control-plaintext" value="<?php echo $customerName; ?>"">
 									</div>
 								</div>
 								<div role="group" class="form-group form-row">
 									<label for="contactLastName" class="col-form-label col-sm-3">contactLastName</label>
 									<div class="col-sm-9">
-										<input id="contactLastName" type="text" readonly="readonly" class="form-control-plaintext" value="<?php echo $contactLastName; ?>"">
+										<input id="contactLastName" type="text"  class="form-control-plaintext" value="<?php echo $contactLastName; ?>"">
 									</div>
 								</div>
 								<div role="group" class="form-group form-row">
 									<label for="contactFirstName" class="col-form-label col-sm-3">contactFirstName</label>
 									<div class="col-sm-9">
-										<input id="contactFirstName" type="text" readonly="readonly" class="form-control-plaintext" value="<?php echo $contactFirstName; ?>"">
+										<input id="contactFirstName" type="text"  class="form-control-plaintext" value="<?php echo $contactFirstName; ?>"">
 									</div>
 								</div>
 								<div role="group" class="form-group form-row">
 									<label for="phone" class="col-form-label col-sm-3">phone</label>
 									<div class="col-sm-9">
-										<input id="phone" type="text" readonly="readonly" class="form-control-plaintext" value="<?php echo $phone; ?>"">
+										<input id="phone" type="text"  class="form-control-plaintext" value="<?php echo $phone; ?>"">
 									</div>
 								</div>
 								<div role="group" class="form-group form-row">
 									<label for="addressLine1" class="col-form-label col-sm-3">addressLine1</label>
 									<div class="col-sm-9">
-										<input id="addressLine1" type="text" readonly="readonly" class="form-control-plaintext" value="<?php echo $addressLine1; ?>"">
+										<input id="addressLine1" type="text"  class="form-control-plaintext" value="<?php echo $addressLine1; ?>"">
 									</div>
 								</div>
 								<div role="group" class="form-group form-row">
 									<label for="addressLine2" class="col-form-label col-sm-3">addressLine2</label>
 									<div class="col-sm-9">
-										<input id="addressLine2" type="text" readonly="readonly" class="form-control-plaintext" value="<?php echo $addressLine2; ?>"">
+										<input id="addressLine2" type="text"  class="form-control-plaintext" value="<?php echo $addressLine2; ?>"">
 									</div>
 								</div>
 								<div role="group" class="form-group form-row">
 									<label for="city" class="col-form-label col-sm-3">city</label>
 									<div class="col-sm-9">
-										<input id="city" type="text" readonly="readonly" class="form-control-plaintext" value="<?php echo $city; ?>"">
+										<input id="city" type="text"  class="form-control-plaintext" value="<?php echo $city; ?>"">
 									</div>
 								</div>
 								<div role="group" class="form-group form-row">
 									<label for="state" class="col-form-label col-sm-3">state</label>
 									<div class="col-sm-9">
-										<input id="state" type="text" readonly="readonly" class="form-control-plaintext" value="<?php echo $state; ?>"">
+										<input id="state" type="text"  class="form-control-plaintext" value="<?php echo $state; ?>"">
 									</div>
 								</div>
 								<div role="group" class="form-group form-row">
 									<label for="postalCode" class="col-form-label col-sm-3">postalCode</label>
 									<div class="col-sm-9">
-										<input id="postalCode" type="text" readonly="readonly" class="form-control-plaintext" value="<?php echo $postalCode; ?>"">
+										<input id="postalCode" type="text"  class="form-control-plaintext" value="<?php echo $postalCode; ?>"">
 									</div>
 								</div>
 								<div role="group" class="form-group form-row">
 									<label for="country" class="col-form-label col-sm-3">country</label>
 									<div class="col-sm-9">
-										<input id="country" type="text" readonly="readonly" class="form-control-plaintext" value="<?php echo $country; ?>"">
+										<input id="country" type="text"  class="form-control-plaintext" value="<?php echo $country; ?>"">
 									</div>
 								</div>
 								<div role="group" class="form-group form-row">
 									<label for="salesRepEmployeeNumber" class="col-form-label col-sm-3">salesRepEmployeeNumber</label>
 									<div class="col-sm-9">
-										<input id="salesRepEmployeeNumber" type="text" readonly="readonly" class="form-control-plaintext" value="<?php echo $salesRepEmployeeNumber; ?>"">
+										<input id="salesRepEmployeeNumber" type="text"  class="form-control-plaintext" value="<?php echo $salesRepEmployeeNumber; ?>"">
 									</div>
 								</div>
 								<div role="group" class="form-group form-row">
 									<label for="creditLimit" class="col-form-label col-sm-3">creditLimit</label>
 									<div class="col-sm-9">
-										<input id="creditLimit" type="text" readonly="readonly" class="form-control-plaintext" value="<?php echo $creditLimit; ?>"">
+										<input id="creditLimit" type="text"  class="form-control-plaintext" value="<?php echo $creditLimit; ?>"">
 									</div>
 								</div>
 
